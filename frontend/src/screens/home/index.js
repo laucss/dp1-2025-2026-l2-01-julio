d843a3bc38a7a0820726acca6759aaaaa8c5e7ea
nereeea13 <nereamix2005@gmail.com>
2025-10-23 09:57:54 +0200

    Mejora pantalla inicial y creacion pantallas createMatch y JoinMatch

--- frontend/src/screens/home/index.js
+++ frontend/src/screens/home/index.js
@@ -1,29 +1,22 @@
 import React from 'react';
-import './home.css';
 import '../../App.css';
-import '../../static/css/home/home.css'; 
-import logo from '../../static/images/napoleon.png'
+import '../../static/css/home/home.css';
+import { useNavigate } from 'react-router-dom'; 
 
 export default function Home(){
+    const navigate = useNavigate();
     return(
         <div className="home-page-container">
-            <div className='home-container'>
-                <h1 className='home-title'>
-                    ESCAPE FROM ELBA
-                </h1>            
-                <div className='home-buttons'>
-                    <button className='create-button'>
+            <div className='home-buttons'>
+                <button className='create-button' onClick={() => navigate('/matchs/new')}>
                         Crear partida  
-                    </button>  
-                    <button className='join-button'>
+                </button>  
+                <button className='join-button'onClick={() => navigate('/lobbies')}>
                         Unirse a partida  
-                    </button> 
-
-                </div>
-                
+                </button>
             </div>
-         <img src={logo} className="napoleon-div" /> 
         </div>
+
     );
 
 
---
+++ frontend/src/screens/match/joinMatch.js
@@ -0,0 +1,10 @@
+export default function JoinMatch(){
+
+    return (
+        <div>
+            <div className="rules-header">
+                <h1 className="text-center"> JoinMatch </h1>
+            </div>
+        </div>
+            )
+    }
