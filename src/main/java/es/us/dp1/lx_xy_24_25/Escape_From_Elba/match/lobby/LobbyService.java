9957050ba8ca98ea327c2e607a97a7be77413641
nereeea13 <nereamix2005@gmail.com>
2025-10-26 20:59:44 +0100

    funciones para buscar lobbies publicos y lobbie privado y borrador de pantalla joinMatch

--- src/main/java/es/us/dp1/lx_xy_24_25/Escape_From_Elba/match/lobby/LobbyService.java
+++ src/main/java/es/us/dp1/lx_xy_24_25/Escape_From_Elba/match/lobby/LobbyService.java
@@ -1,10 +1,37 @@
 package es.us.dp1.lx_xy_24_25.Escape_From_Elba.match.lobby;
 
-import es.us.dp1.lx_xy_24_25.Escape_From_Elba.match.MatchRepository;
+import es.us.dp1.lx_xy_24_25.Escape_From_Elba.match.Match;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.us.dp1.lx_xy_24_25.Escape_From_Elba.match.MatchRepository;
+@Service
 public class LobbyService {
 
     MatchRepository mrepo;
+
+    @Autowired
+    public LobbyService(MatchRepository mrepo) {
+        this.mrepo = mrepo;
+    }
+
     
+    @Transactional(readOnly = true)
+    public List<Match> getAllPublicLobbies() {
+        return mrepo.findPublicLobbies();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Match> getPrivateLobby(String codeLobby) {
+        return mrepo.findPrivateLobbieById(codeLobby);
+    }
     
 }
