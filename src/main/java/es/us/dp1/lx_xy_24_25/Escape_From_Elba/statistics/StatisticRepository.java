a82236734231effb8d046b197d5f72981483e092
Emilio <emiliodiazarcenegui@gmail.com>
2025-11-13 20:00:59 +0100

    Errores incomprensibles

--- src/main/java/es/us/dp1/lx_xy_24_25/Escape_From_Elba/statistics/StatisticRepository.java
+++ src/main/java/es/us/dp1/lx_xy_24_25/Escape_From_Elba/statistics/StatisticRepository.java
@@ -1,12 +1,10 @@
 package es.us.dp1.lx_xy_24_25.Escape_From_Elba.statistics;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public class StatisticRepository extends CrudRepository<Statistic, Integer>{
-    
-    List<Statistic> findAll();
-
+@Repository
+public interface StatisticRepository extends CrudRepository<Statistic, Integer> {
 }
