ff7e569032629bbe20a7c0709c402d9f9d03611a
nereeea13 <nereamix2005@gmail.com>
2025-11-19 18:56:44 +0100

    Frontend lobby

--- src/main/java/es/us/dp1/lx_xy_24_25/Escape_From_Elba/cards/deck/DeckService.java
+++ src/main/java/es/us/dp1/lx_xy_24_25/Escape_From_Elba/cards/deck/DeckService.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,7 +35,7 @@ public class DeckService {
     public Checkers checkers;
 
     @Autowired
-    public DeckService(CardRepository cardRepository, Checkers checkers) {
+    public DeckService(CardRepository cardRepository, @Lazy Checkers checkers) {
         this.cardRepository = cardRepository;
         this.checkers = checkers; 
     }
