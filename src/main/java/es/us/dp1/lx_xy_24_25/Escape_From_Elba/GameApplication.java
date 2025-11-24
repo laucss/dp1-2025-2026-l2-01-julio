0a65f4d0e74741d156df97bc87673a8141dff0be
laucss <laucubsan@alum.us.es>
2025-11-24 21:00:53 +0100

    commit para ver un error

--- src/main/java/es/us/dp1/lx_xy_24_25/Escape_From_Elba/GameApplication.java
+++ src/main/java/es/us/dp1/lx_xy_24_25/Escape_From_Elba/GameApplication.java
@@ -2,6 +2,9 @@ package es.us.dp1.lx_xy_24_25.Escape_From_Elba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication()
 public class GameApplication {
@@ -9,5 +12,8 @@ public class GameApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GameApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {return builder.build();}
 
 }
