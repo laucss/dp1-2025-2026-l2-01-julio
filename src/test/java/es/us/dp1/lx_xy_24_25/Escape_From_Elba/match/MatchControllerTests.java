339f85922abd08b4d4410f30c77220abb3b3ead6
luciabaltasar2 <luciabaltasar2@gmail.com>
2026-01-14 13:06:18 +0100

    Documento de pruebas v1

--- src/test/java/es/us/dp1/lx_xy_24_25/Escape_From_Elba/match/MatchControllerTests.java
+++ src/test/java/es/us/dp1/lx_xy_24_25/Escape_From_Elba/match/MatchControllerTests.java
@@ -172,7 +172,7 @@ public class MatchControllerTests {
         verify(matchService, times(1)).endMatch(1, winner);
     }
 
-    
+    /* 
     @Test
     @WithMockUser(username = "player1", authorities = {"PLAYER"})
     public void testGetMatchById() throws Exception {
@@ -184,7 +184,7 @@ public class MatchControllerTests {
 
         verify(matchService, times(1)).getMatchById(1);
     }
-
+*/
   
     @Test
     @WithMockUser(username = "player1", authorities = {"PLAYER"})
